cabe633fb646e104be890803de29024b16c16095
Sayed Atif <sayedatif86@gmail.com>
2024-02-13 14:07:16 +0530

    add slash route

--- server/router.go
+++ server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sayedatif/tigerhall/controllers/tigers"
 	"github.com/sayedatif/tigerhall/controllers/users"
@@ -14,6 +16,11 @@ import (
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "Endpoint healthy",
+		})
+	})
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	database := db.GetDB()
